8cca5c85c82d1cd8440249fd2d0371bcfc617ae4
Chloe Revery <chloe@yikyakapp.com>
2016-11-12 15:51:00 -0800

    Add in database connection

---
+++ src/db/sqlite.go
@@ -0,0 +1,90 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type TestItem struct {
+	MAC               string
+	Compromised       string
+	TimeClassifiedUTC string
+	Description       string
+	Action            string
+	Success           string
+	PacketID          string
+}
+
+func CreateTable(db *sql.DB) {
+	// create table if not exists
+	sql_table := `
+	CREATE TABLE IF NOT EXISTS packets(
+		MAC TEXT,
+		Compromised TEXT,
+		TimeClassifiedUTC TEXT,
+		Description TEXT,
+		Action TEXT,
+		Success TEXT,
+		PacketID TEXT NOT NULL PRIMARY KEY
+	);
+	`
+
+	_, err := db.Exec(sql_table)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func StoreItem(db *sql.DB, item TestItem) error {
+	fmt.Println("item.PacketID", item.PacketID)
+
+	sql_additem := `
+	INSERT OR REPLACE INTO packets(
+		MAC,
+		Compromised,
+		TimeClassifiedUTC,
+		Description,
+		Action,
+		Success,
+		PacketID
+	) values(?, ?, ?, ?, ?, ?, ?)
+	`
+
+	stmt, err := db.Prepare(sql_additem)
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+
+	_, err2 := stmt.Exec(item.MAC, item.Compromised, item.TimeClassifiedUTC, item.Description, item.Action, item.Success, item.PacketID)
+	if err2 != nil {
+		panic(err2)
+	}
+
+	return nil
+}
+
+func ReadItem(db *sql.DB) []TestItem {
+	sql_readall := `
+	SELECT MAC, Compromised, TimeClassifiedUTC, Description, Action, Success, PacketID FROM packets
+	ORDER BY PacketID DESC
+	`
+
+	rows, err := db.Query(sql_readall)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	var result []TestItem
+	for rows.Next() {
+		item := TestItem{}
+		err2 := rows.Scan(&item.MAC, &item.Compromised, &item.TimeClassifiedUTC, &item.Description, &item.Action, &item.Success, &item.PacketID)
+		if err2 != nil {
+			panic(err2)
+		}
+		result = append(result, item)
+	}
+	return result
+}
--- src/main.go
+++ src/main.go
@@ -1,29 +1,52 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "sync"
+	"database/sql"
+	"fmt"
+	"math/rand"
+	"strconv"
+	"sync"
 	"time"
-	
+
 	"kanashi/src/analyzer"
-	
+	database "kanashi/src/db"
+
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
+
+	"github.com/mattn/go-sqlite3"
 )
 
 const (
-    pcapFileSrc = "../testdata/test_pcap.pcap"
-    
-    SECOND_FRAC = 10
+	pcapFileSrc = "../testdata/test_pcap.pcap"
+
+	SECOND_FRAC = 10
 )
 
 func init() {
-    rand.Seed(time.Now().UTC().UnixNano())
+	rand.Seed(time.Now().UTC().UnixNano())
 }
 
 func main() {
-    // Initialize traffic analysis struct to check for destination IP spikes.
+
+	// Register database driver.
+	var DB_DRIVER string
+	sql.Register(DB_DRIVER, &sqlite3.SQLiteDriver{})
+
+	// Open database connection.
+	db, err := sql.Open(DB_DRIVER, "mysqlite_3")
+	if err != nil {
+		panic(err)
+	}
+	if db == nil {
+		panic("db nil")
+	}
+	defer db.Close()
+
+	// Create table to store packet info and decisions.
+	database.CreateTable(db)
+
+	// Initialize traffic analysis struct to check for destination IP spikes.
 	uniqueDestIps := &analyzer.UniqueDestIps{
 		Mutex:        &sync.Mutex{},
 		IPs:          make(map[string]analyzer.Empty),
@@ -36,76 +59,86 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	
+
 	// Construct packetSource using pcap file.
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-	
+
+	var count int
+
 	for packet := range packetSource.Packets() {
+		count++
 		for _, packetLayer := range packet.Layers() {
 			result := analyzer.PeelLayer(packetLayer)
-			
+
 			if result == analyzer.MALICIOUS {
-			    err := logPacketAsMalicious(packet)
-			    if err != nil {
-			        panic(err)
-			    }
-			    
-			    break
+				err := logPacketAsMalicious(packet)
+				if err != nil {
+					panic(err)
+				}
+
+				break
 			}
-			
-			err := logPacketInfo(packet)
+
+			err := logPacketInfo(packet, result, db)
 			if err != nil {
-			    panic(err)
+				panic(err)
 			}
-			
+
 			err = sendPacketThru(packet)
 			if err != nil {
-			    panic(err)
+				panic(err)
 			}
 		}
 
-        // FOR STATIC PCAP TESTING PURPOSES.
-        // Generates a random sleep duration between (1/(SECOND_FRAC+1)) and 1s.
-        sleepDuration := time.Second / time.Duration(rand.Intn(SECOND_FRAC)+1)
+		// FOR STATIC PCAP TESTING PURPOSES.
+		// Generates a random sleep duration between (1/(SECOND_FRAC+1)) and 1s.
+		sleepDuration := time.Second / time.Duration(rand.Intn(SECOND_FRAC)+1)
 		time.Sleep(sleepDuration)
+		fmt.Println("count:", count)
+		fmt.Println("CONTENTS OF DATABASE", database.ReadItem(db))
 	}
 }
 
-// Gens packet info, timestamp, success of packet drop, additional metadata..
-func generatePacketInfo(packet gopacket.Packet) (string, error) {
-    return "packet was good", nil
-}
+var count = 1
 
-func logPacketInfo(packet gopacket.Packet) error {
-    // TODO: Grab the first param, to-contain packet info.
-    _, err := generatePacketInfo(packet)
-    if err != nil {
-        return err
-    }
-    
-    // TODO: Hit analytics with this info.
-    return nil
+// Writes packet metadata to database.
+func logPacketInfo(packet gopacket.Packet, result string, db *sql.DB) error {
+
+	fmt.Println("count:", count)
+	item := &database.TestItem{
+		MAC:               "TEST MAC",
+		Compromised:       "false",
+		TimeClassifiedUTC: "0",
+		Description:       result,
+		Action:            "TEST ACTION",
+		Success:           "TEST SUCCESS",
+		PacketID:          strconv.Itoa(count),
+	}
+
+	count++
+
+	err := database.StoreItem(db, *item)
+	if err != nil {
+		return err
+	}
+
+	// TODO: Hit analytics with this info.
+	return nil
 }
 
 func logPacketAsMalicious(packet gopacket.Packet) error {
-    // TODO: Grab the first param, to-contain packet info.
-    _, err := generatePacketInfo(packet)
-    if err != nil {
-        return err
-    }
-    
-    fmt.Printf("REJECTED PACKET: %+v\n", packet)
-    
-    // info = "packet was bad"
-    
-    // TODO: Hit analytics with this info.
-    return nil
+	// TODO: Grab the first param, to-contain packet info.
+
+	fmt.Printf("REJECTED PACKET: %+v\n", packet)
+
+	// info = "packet was bad"
+
+	// TODO: Hit analytics with this info.
+	return nil
 }
 
 func sendPacketThru(packet gopacket.Packet) error {
-    // TODO: Send packet to its destination.
-    fmt.Printf("SENDING PACKET: %+v\n", packet)
-    return nil
+	// TODO: Send packet to its destination.
+	fmt.Printf("SENDING PACKET: %+v\n", packet)
+	return nil
 }
-
-
