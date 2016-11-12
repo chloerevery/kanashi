2afff574e54c3b41ecb263bb1e57af595c5a0b1f
bern422 <bern422@gmail.com>
2016-11-12 05:33:33 -0500

    first draft of tcp check

--- src/analyzer/analyzer.go
+++ src/analyzer/analyzer.go
@@ -13,31 +13,20 @@ func PeelLayers(packet gopacket.Packet) {
 		-DNS requests: read the IP address of every DNS destination.
 	*/
 
+    // Check for malicious TCP.
 	for _, onionLayer := range packet.Layers() {
-
-		switch onionLayer.LayerType() {
-		case layers.LayerTypeTCP:
-			fmt.Println("This is a TCP packet!")
-			// Get actual TCP data from this layer
-			tcp, _ := onionLayer.(*layers.TCP)
-
-			fmt.Printf("TCP: From src port %d to dst port %d\n", tcp.SrcPort, tcp.DstPort)
-		case layers.LayerTypeIPv4:
-			fmt.Println("This is an ipv4 packet!")
-			// Get actual IP header + data from this layer
-			ip, _ := onionLayer.(*layers.IPv4)
-
-			fmt.Printf("IPv4: From src ip %d to dst ip %d\n", ip.SrcIP, ip.DstIP)
-		case layers.LayerTypeUDP:
-			fmt.Println("This is a UDP packet!")
-			// Get actual IP header + data from this layer
-			udp, _ := onionLayer.(*layers.UDP)
-
-			fmt.Printf("UDP: From src port %d to dst port %d\n", udp.SrcPort, udp.DstPort)
-			// TODO: UDP
-
-		}
-
-		// TODO: Get data from other layers.
-	}
+        if onionLayer.LayerType() == layers.LayerTypeIPv4 {
+            ip, _ := onionLayer.(*layers.IPv4)
+            
+            if ip.Protocol == layers.IPProtocolTCP {
+                fmt.Println("Uses TCP as protocol!")
+                fmt.Printf("SrcIP: %+v *** DstIP: %+v\n", ip.SrcIP, ip.DstIP)
+                for _, maliciousIP := range maliciousIPs {
+                    if ip.DstIP.String() == maliciousIP {
+                        fmt.Println("SHIT SHIT SHIT")
+                    }
+                }
+            }
+        }
+   }
 }
