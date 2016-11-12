f1fc685602330bc482b2929d237361502595218f
Chloe Revery <chloe@yikyakapp.com>
2016-11-12 02:19:28 -0800

    Detect malicious UDP packet

--- src/analyzer/analyzer.go
+++ src/analyzer/analyzer.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
+	"net"
 )
 
 // PeelLayers takes in a packet with a non-zero number of "layers" and iterates through each layer, collecting data.
@@ -27,10 +28,28 @@ func PeelLayers(packet gopacket.Packet) {
 			// Get actual IP header + data from this layer
 			ip, _ := onionLayer.(*layers.IPv4)
 
+			fmt.Println("CHLOE, src ip:", ip.SrcIP)
+
+			// Chloe would like someone to confirm that these are the correct values.
+			ipOKMask := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+			ipOK := net.ParseIP("192.168.0.0")
+
+			IpNetOK := &net.IPNet{
+				IP:   ipOK,     // network number
+				Mask: ipOKMask, // network mask
+			}
+
+			IpMalicious := IpNetOK.Contains(ip.SrcIP)
+
+			fmt.Println("IS IP PACKET MALICIOUS?", IpMalicious)
+
+			// If src IP is in range 192.168.0.0 - 192.168.1.16 (?) it's likely ok.
+
 			fmt.Printf("IPv4: From src ip %d to dst ip %d\n", ip.SrcIP, ip.DstIP)
 		case layers.LayerTypeUDP:
 			fmt.Println("This is a UDP packet!")
-			// Get actual IP header + data from this layer
+
+			// Get UDP packet data from this layer.
 			udp, _ := onionLayer.(*layers.UDP)
 
 			fmt.Printf("UDP: From src port %d to dst port %d\n", udp.SrcPort, udp.DstPort)
