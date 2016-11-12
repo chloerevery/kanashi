1d703fda8d129c75d9a802e94955dc159a8b6b0b
Chloe Revery <chloe@yikyakapp.com>
2016-11-12 00:55:44 -0800

    More coding

--- golang/analyzer/analyzer.go
+++ golang/analyzer/analyzer.go
@@ -31,49 +31,41 @@ func peelLayers(packet gopacket.Packet) {
 		-DNS requests: read the IP address of every DNS destination.
 	*/
 
-	for _, layer := range packet.Layers() {
+	for _, onionLayer := range packet.Layers() {
 
-		switch layer.LayerType() {
-		case packet.Layer(layers.LayerTypeTCP):
-
-		case packet.Layer(layers.LayerTypeIPv4):
-		case packet.Layer(layers.LayerTypeIPv6):
-
-		}
-
-		// Get the TCP layer from this packet
-		if layer := packet.Layer(layers.LayerTypeTCP); layer != nil {
+		switch onionLayer.LayerType() {
+		case layers.LayerTypeTCP:
 			fmt.Println("This is a TCP packet!")
 			// Get actual TCP data from this layer
-			tcp, _ := tcpLayer.(*layers.TCP)
+			tcp, _ := onionLayer.(*layers.TCP)
 
 			// Decode a TCP header and its payload (CHLOE: What does this do?)
-			tcpP := gopacket.NewPacket(packet, layers.LayerTypeTCP, gopacket.Default)
+			// TODO: Determine if below code is needed.
+			// tcpP := gopacket.NewPacket(packet, layers.LayerTypeTCP, gopacket.Default)
 
-			fmt.Printf("From src port %d to dst port %d\n", tcp.SrcPort, tcp.DstPort)
-		}
-
-		if ip6Layer := packet.Layer(layers.LayerTypeIPv6); ip6Layer != nil {
-			fmt.Println("This is an ipv6 packet!")
-			// Get actual IP header + data from this layer
-			ip, _ := ip6Layer.(*layers.IPV6)
-
-			// Decode an IPv6 header and everything it contains (CHLOE: What does this do?)
-			ipP := gopacket.NewPacket(packet, layers.LayerTypeIPv6, gopacket.Default)
-
-			fmt.Printf("From src port %d to dst port %d\n", ip.SrcPort, ip.DstPort)
-		}
-
-		if ip4Layer := packet.Layer(layers.LayerTypeIPv4); ip4Layer != nil {
+			fmt.Printf("TCP: From src port %d to dst port %d\n", tcp.SrcPort, tcp.DstPort)
+		case layers.LayerTypeIPv4:
 			fmt.Println("This is an ipv4 packet!")
 			// Get actual IP header + data from this layer
-			ip, _ := ip4Layer.(*layers.IPV4)
+			ip, _ := onionLayer.(*layers.IPv4)
 
 			// Decode an IPv4 header and everything it contains (CHLOE: What does this do?)
-			ipP := gopacket.NewPacket(packet, layers.LayerTypeIPv4, gopacket.Default)
+			// TODO: Determine if below code is needed.
+			// ipP := gopacket.NewPacket(packet, layers.LayerTypeIPv4, gopacket.Default)
 
-			fmt.Printf("From src port %d to dst port %d\n", ip.SrcPort, ip.DstPort)
+			fmt.Printf("IPv4: From src ip %d to dst ip %d\n", ip.SrcIP, ip.DstIP)
+		case layers.LayerTypeIPv6:
+			fmt.Println("This is an ipv6 packet!")
+			// Get actual IP header + data from this layer
+			ip, _ := onionLayer.(*layers.IPv6)
+
+			// Decode an IPv6 header and everything it contains (CHLOE: What does this do?)
+			// TODO: Determine if below code is needed.
+			// ipP := gopacket.NewPacket(packet, layers.LayerTypeIPv6, gopacket.Default)
+
+			fmt.Printf("IPv6: From src ip %d to dst ip %d\n", ip.SrcIP, ip.DstIP)
 		}
+
 	}
 
 	// TODO: Get data from other layers.
