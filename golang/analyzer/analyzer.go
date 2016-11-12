f7efe50081f006334d0e8044a508f02a8129fc5c
Chloe Revery <chloe@yikyakapp.com>
2016-11-12 00:24:26 -0800

    Simplify logic

--- golang/analyzer/analyzer.go
+++ golang/analyzer/analyzer.go
@@ -18,25 +18,20 @@ func main() {
 	} else {
 		packetSource := gopacket.NewPacketSource(handle, handle.LinkType()) // construct packetSource using pcap or pfring
 		for packet := range packetSource.Packets() {
-			decode(packet) // do something with each packet
+			// Packets fn returns a channel, then asynchronously writes new packets into that channel, closing the channel if the packetSource hits an end-of-file.
+			handlePacket(packet) // do something with each packet
 		}
 	}
 }
 
-// decode takes in packet data as a []byte and decodes it into a packet with a non-zero number of "layers".
-func decode(myPacketData []byte) {
-	// Decode a packet
-	packet := gopacket.NewPacket(myPacketData, layers.LayerTypeEthernet, gopacket.Default)
-	// Get the TCP layer from this packet
+// handlePacket takes in a packet with a non-zero number of "layers".
+func handlePacket(packet gopacket.Packet) {
+	// Sample: Get the TCP layer from this packet
 	if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
 		fmt.Println("This is a TCP packet!")
 		// Get actual TCP data from this layer
 		tcp, _ := tcpLayer.(*layers.TCP)
 		fmt.Printf("From src port %d to dst port %d\n", tcp.SrcPort, tcp.DstPort)
 	}
-	// Iterate over all layers, printing out each layer type
-	for _, layer := range packet.Layers() {
-		// TODO: Decode each layer.
-		fmt.Println("PACKET LAYER:", layer.LayerType())
-	}
+	// TODO: Get data from other layers.
 }
