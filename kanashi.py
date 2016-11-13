c3f89d86889c4718e4f9f714a916fedb7e6a21fc
lizzy <lizzy@evident.io>
2016-11-12 17:53:00 -0800

    inital commit

---
+++ kanashi.py
@@ -0,0 +1,32 @@
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+    render_template, flash
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'kanashi.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+@app.route('/')
+def starting_page():
+	return render_template('index.html')
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+if __name__ == '__main__':
+	app.debug=True
+	connect_db()
+	app.run()
+
+
